9f641ded7ffa1db9cb77fda6978164aa46faaffa
Marc Palacios Domènech <megamarc@hotmail.com>
2017-05-12 20:54:51 +0100

    DKC Level 1 recreation
    Python example that recreates the first level of SNES Donkey Kong
    Country, with extensive use of raster effects

---
+++ bin/dkc_bg1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dkc_bg1" tilewidth="8" tileheight="8" >
+	<image source="dkc_bg1.png" width="128" height="392"/>
+</tileset>
---
+++ bin/dkc_bg3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dkc_bg3" tilewidth="8" tileheight="8" >
+	<image source="dkc_bg3.png" width="128" height="16"/>
+</tileset>
