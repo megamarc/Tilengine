773faaf3494796440900e2dee32421beac695019
megamarc <megamarc@hotmail.com>
2019-12-03 14:20:17 +0100

    Implements core and basic LUA support
    * Working libretro core: inits tilengine at 480x270 pixels and sets brown background color
    * Integrates LuaBinaries into the core, builds properly under Windows

--- src/libretro/libretro.c
+++ src/libretro/libretro.c
@@ -10,10 +10,9 @@
 #endif
 
 #include <stdio.h>
-#if defined(_WIN32) && !defined(_XBOX)
-#include <windows.h>
-#endif
 #include "libretro.h"
+#include "Tilengine.h"
+#include "lua.h"
 
 #define VIDEO_WIDTH 480
 #define VIDEO_HEIGHT 270
@@ -28,6 +27,15 @@ static float last_sample_rate;
 char retro_base_directory[4096];
 char retro_game_path[4096];
 
+/* tilengine specific */
+#define TLN_NUM_LAYERS		4
+#define TLN_NUM_SPRITES		128
+#define TLN_NUM_ANIMATIONS	32
+
+static TLN_Engine engine;
+static int frame;
+static lua_State* lua_state;
+
 static void fallback_log(enum retro_log_level level, const char *fmt, ...)
 {
 	(void)level;
@@ -41,7 +49,6 @@ static retro_environment_t environ_cb;
 
 void retro_init(void)
 {
-	frame_buf = (uint8_t*)malloc(VIDEO_PIXELS * sizeof(uint32_t));
 	const char *dir = NULL;
 	if (environ_cb(RETRO_ENVIRONMENT_GET_SYSTEM_DIRECTORY, &dir) && dir)
 	{
@@ -51,8 +58,6 @@ void retro_init(void)
 
 void retro_deinit(void)
 {
-	free(frame_buf);
-	frame_buf = NULL;
 }
 
 unsigned retro_api_version(void)
@@ -68,10 +73,10 @@ void retro_set_controller_port_device(unsigned port, unsigned device)
 void retro_get_system_info(struct retro_system_info *info)
 {
 	memset(info, 0, sizeof(*info));
-	info->library_name = "skeleton";
+	info->library_name = "Tilengine";
 	info->library_version = "0.1";
 	info->need_fullpath = true;
-	info->valid_extensions = "";
+	info->valid_extensions = "ini";
 }
 
 static retro_video_refresh_t video_cb;
@@ -82,24 +87,22 @@ static retro_input_state_t input_state_cb;
 
 void retro_get_system_av_info(struct retro_system_av_info *info)
 {
-	float aspect = 0.0f;
-	float sampling_rate = 30000.0f;
-
-
 	info->geometry.base_width = VIDEO_WIDTH;
 	info->geometry.base_height = VIDEO_HEIGHT;
 	info->geometry.max_width = VIDEO_WIDTH;
 	info->geometry.max_height = VIDEO_HEIGHT;
-	info->geometry.aspect_ratio = aspect;
-
-	last_aspect = aspect;
-	last_sample_rate = sampling_rate;
+	info->geometry.aspect_ratio = 0;	/* auto width/height */
+	info->timing.fps = 60;
+	info->timing.sample_rate = 22050;
 }
 
 static struct retro_rumble_interface rumble;
 
 void retro_set_environment(retro_environment_t cb)
 {
+	bool no_rom = true;
+	enum retro_pixel_format fmt = RETRO_PIXEL_FORMAT_XRGB8888;
+
 	environ_cb = cb;
 
 	if (cb(RETRO_ENVIRONMENT_GET_LOG_INTERFACE, &logging))
@@ -117,6 +120,8 @@ void retro_set_environment(retro_environment_t cb)
 	};
 
 	cb(RETRO_ENVIRONMENT_SET_CONTROLLER_INFO, (void*)ports);
+	cb(RETRO_ENVIRONMENT_SET_SUPPORT_NO_GAME, &no_rom);
+	cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &fmt);
 }
 
 void retro_set_audio_sample(retro_audio_sample_t cb)
@@ -144,16 +149,13 @@ void retro_set_video_refresh(retro_video_refresh_t cb)
 	video_cb = cb;
 }
 
-static unsigned x_coord;
-static unsigned y_coord;
 static unsigned phase;
 static int mouse_rel_x;
 static int mouse_rel_y;
 
 void retro_reset(void)
 {
-	x_coord = 0;
-	y_coord = 0;
+
 }
 
 static void update_input(void)
@@ -189,40 +191,51 @@ void retro_run(void)
 	bool updated = false;
 	if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE_UPDATE, &updated) && updated)
 		check_variables();
+
+	TLN_UpdateFrame(frame);
+	video_cb(frame_buf, VIDEO_WIDTH, VIDEO_HEIGHT, VIDEO_WIDTH * sizeof(uint32_t));
+	frame += 1;
 }
 
 bool retro_load_game(const struct retro_game_info *info)
 {
 	struct retro_input_descriptor desc[] = {
-	   { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_LEFT,  "Left" },
-	   { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP,    "Up" },
-	   { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_DOWN,  "Down" },
-	   { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_RIGHT, "Right" },
-	   { 0 },
+	  { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_LEFT,  "Left" },
+	  { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP,    "Up" },
+	  { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_DOWN,  "Down" },
+	  { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_RIGHT, "Right" },
+	  { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_B,     "2" },
+	  { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_A,     "1" },
+	  { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_X,     "3" },
+	  { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_Y,     "4" },
+	  { 0 },
 	};
 
 	environ_cb(RETRO_ENVIRONMENT_SET_INPUT_DESCRIPTORS, desc);
 
-	enum retro_pixel_format fmt = RETRO_PIXEL_FORMAT_XRGB8888;
-	if (!environ_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &fmt))
-	{
-		log_cb(RETRO_LOG_INFO, "XRGB8888 is not supported.\n");
-		return false;
-	}
-
 	snprintf(retro_game_path, sizeof(retro_game_path), "%s", info->path);
 	struct retro_audio_callback audio_cb = { audio_callback, audio_set_state };
-	use_audio_cb = environ_cb(RETRO_ENVIRONMENT_SET_AUDIO_CALLBACK, &audio_cb);
+	//use_audio_cb = environ_cb(RETRO_ENVIRONMENT_SET_AUDIO_CALLBACK, &audio_cb);
 
 	check_variables();
 
 	(void)info;
+
+	/* Tilengine */
+	frame_buf = (uint8_t*)malloc(VIDEO_PIXELS * sizeof(uint32_t));
+	engine = TLN_Init(VIDEO_WIDTH, VIDEO_HEIGHT, TLN_NUM_LAYERS, TLN_NUM_SPRITES, TLN_NUM_ANIMATIONS);
+	TLN_SetRenderTarget(frame_buf, VIDEO_WIDTH * sizeof(uint32_t));
+	TLN_SetBGColor(128, 64, 32);
+	frame = 0;
+
 	return true;
 }
 
 void retro_unload_game(void)
 {
-
+	TLN_Deinit();
+	free(frame_buf);
+	frame_buf = NULL;
 }
 
 unsigned retro_get_region(void)
