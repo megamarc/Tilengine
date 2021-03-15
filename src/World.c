c3f89d42e303ff38529cc537c9408a9f7e479e6d
megamarc <megamarc@hotmail.com>
2021-03-15 23:40:48 +0100

    Fixes Color definition (typedef union) on Linux

--- src/World.c
+++ src/World.c
@@ -6,14 +6,15 @@
 
 #define MAX_TMX_ITEM	100
 
-union Color
+typedef union
 {
 	uint32_t value;
 	struct
 	{
 		uint8_t b, g, r, a;
 	};
-};
+}
+Color;
 
 /* info for current world */
 static TMXInfo tmxinfo;
